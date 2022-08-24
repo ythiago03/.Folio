90b461b4990968f9e0732d82f352abdf7de4db64
Thiago Henrique <92443688+ythiago03@users.noreply.github.com>
2022-08-24 12:08:40 -0300

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,11 @@
+const containerMobile = document.querySelector(".container-mobile")
+
+
+
 function clicou(){
     const navbar = document.getElementById('navbar')
-    const checkbox = document.getElementById('checkbox-menu')
-    const menuMobile = document.getElementById('container-mobile')
-
-        
-    
-        if(checkbox.checked == true){
-            navbar.style.display = 'block'
-            console.log('true')
- 
-        }else{
-            navbar.style.display = 'none'
-            console.log('false')
-        }
+    if(containerMobile.style.display === 'none'){
+        navbar.classList.remove('open')  
+    }
+    navbar.classList.toggle('open')    
 }
